05e3ca82ac104964120205f1b79005a45addd4e5
Art Furrer <2823246+bonham@users.noreply.github.com>
2020-09-06 01:02:41 +0200

    temp change for development

--- client/cmain.js
+++ client/cmain.js
@@ -1,7 +1,7 @@
 import { getParams } from "./helpers"
 import { chartConfigurations } from "./chartconfigs"
-import Chart from 'chart.js';
-
+// import Chart from 'chart.js';
+import Chart from './Chart.js/src/index'
 
 let p = getParams(window.location.href)
 var chartId = p['id']
